6fdb7d915c24f989009862841ffc3ce05c660021
olof3 <olof3@users.noreply.github.com>
2021-05-25 16:29:41 +0200

    Avoid view allocations for real sylv methods.

--- src/sylv_2x2.jl
+++ src/sylv_2x2.jl
@@ -30,33 +30,32 @@ Find the solution `X` to the continuous-time Sylvester equation
 
 `AX + XB = C`
 
-for small matrices (1x1, 1x2, 2x1, 2x2), overwriting the input `C`.
+for small matrices (1x1, 1x2, 2x1, 2x2),
+overwriting the input `C` with the solution `X`.
 """
 @inline function _sylvc!(A::AbstractMatrix, B::AbstractMatrix, C::AbstractMatrix)
     M, N = size(C)
     if M == 2 && N == 2
-        _sylvc!(A, B, C, Val(2), Val(2))
+        C[:] .= _sylvc(SMatrix{2,2}(A), SMatrix{2,2}(B), SMatrix{2,2}(C))
     elseif M == 2 && N == 1
-        _sylvc!(A, B, C, Val(2), Val(1))
+        C[:] .= _sylvc(SMatrix{2,2}(A), SMatrix{1,1}(B), SMatrix{2,1}(C))
     elseif M == 1 && N == 2
-        _sylvc!(A, B, C, Val(1), Val(2))
+        C[:] .= _sylvc(SMatrix{1,1}(A), SMatrix{2,2}(B), SMatrix{1,2}(C))
     elseif M == 1 && N == 1
-        _sylvc!(A, B, C, Val(1), Val(1))
+        C[:] .= _sylvc(SMatrix{1,1}(A), SMatrix{1,1}(B), SMatrix{1,1}(C))
     else
-        error("Matrix dimensionsins should not be greater than 2")
+        error("Problem size must not be greater than (2,2)")
     end
     return C
 end
-@inline function _sylvc!(A::AbstractMatrix, B::AbstractMatrix, C::AbstractMatrix, ::Val{M}, ::Val{N}) where {T <: Number, M, N}
-    As = SMatrix{M,M}(A)
-    Bs = SMatrix{N,N}(B)
-    Cvs = SMatrix{M,N}(C)[:] # vectorization of C
+@inline function _sylvc(A::SMatrix, B::SMatrix, C::SMatrix)
+    M, N = size(C)
 
-    Xv = lu(kron(SMatrix{N,N}(I), As) + kron(transpose(Bs), SMatrix{M,M}(I))) \ Cvs # using the vectorization identity vec(AXB) = kron(B'*A)*vec(X) (with A = I or B = I)
+    Xv = lu(kron(SMatrix{N,N}(I), A) + kron(transpose(B), SMatrix{M,M}(I))) \ C[:] # using the vectorization identity vec(AXB) = kron(B'*A)*vec(X) (with A = I or B = I)
 
     if any(!isfinite, Xv); error("Matrix equation has no solution, see ?sylvc or ?lyapc"); end
 
-    C .= reshape(Xv, M, N)
+    return Xv
 end
 
 
@@ -67,25 +66,27 @@ Find the solution `X` to the discrete-time Sylvester equation
 
 `AXB - X = C`
 
-for small matrices (1x1, 1x2, 2x1, 2x2), overwriting the input `C`.
+for small matrices (1x1, 1x2, 2x1, 2x2),
+overwriting the input `C` with the solution `X`.
 """
 function _sylvd!(A::AbstractMatrix, B::AbstractMatrix, C::AbstractMatrix)    
     M, N = size(C)
-    @inbounds if M == 2 && N == 2
-        C[:] .= _sylvd2(SMatrix{2,2}(A), SMatrix{2,2}(B), SMatrix{2,2}(C))
+    if M == 2 && N == 2
+        C[:] .= _sylvd(SMatrix{2,2}(A), SMatrix{2,2}(B), SMatrix{2,2}(C))
     elseif M == 2 && N == 1
-        C[:] .= _sylvd2(SMatrix{2,2}(A), SMatrix{1,1}(B), SMatrix{2,1}(C))
+        C[:] .= _sylvd(SMatrix{2,2}(A), SMatrix{1,1}(B), SMatrix{2,1}(C))
     elseif M == 1 && N == 2
-        C[:] .= _sylvd2(SMatrix{1,1}(A), SMatrix{2,2}(B), SMatrix{1,2}(C))
+        C[:] .= _sylvd(SMatrix{1,1}(A), SMatrix{2,2}(B), SMatrix{1,2}(C))
     elseif M == 1 && N == 1
-        C[:] .= _sylvd2(SMatrix{1,1}(A), SMatrix{1,1}(B), SMatrix{1,1}(C))
+        C[:] .= _sylvd(SMatrix{1,1}(A), SMatrix{1,1}(B), SMatrix{1,1}(C))
     else
-        error("Matrix dimensionsins should not be greater than 2")
+        error("Problem size must not be greater than (2,2)")
     end
+    return C
 end
-function _sylvd2(A::SMatrix, B::SMatrix, C::SMatrix)
-
+function _sylvd(A::SMatrix, B::SMatrix, C::SMatrix)
     M, N = size(C)
+
     Xv = (kron(transpose(B), A) - SMatrix{M*N,M*N}(I)) \ C[:] # using the vectorization identity vec(AXB) = kron(B'*A)*vec(X)
 
     if any(!isfinite, Xv); error("Matrix equation has no solution, see ?sylvd or ?lyapd"); end
@@ -102,33 +103,29 @@ Find the solution `X` to the discrete-time Sylvester equation
 
 `AXB + EXF = C`
 
-for small matrices (1x1, 1x2, 2x1, 2x2), overwriting the input `C`.
+for small matrices (1x1, 1x2, 2x1, 2x2)
+overwriting the input `C` with the solution `X`.
 """
 function _sylvg!(A::AbstractMatrix, B::AbstractMatrix, C::AbstractMatrix, E::AbstractMatrix, F::AbstractMatrix)
     M, N = size(C)
     if M == 2 && N == 2
-        _sylvg!(A, B, C, E, F, Val(2), Val(2))
+        C[:] .= _sylvg(SMatrix{2,2}(A), SMatrix{2,2}(B), SMatrix{2,2}(C), SMatrix{2,2}(E), SMatrix{2,2}(F))
     elseif M == 2 && N == 1
-        _sylvg!(A, B, C, E, F, Val(2), Val(1))
+        C[:] .= _sylvg(SMatrix{2,2}(A), SMatrix{1,1}(B), SMatrix{2,1}(C), SMatrix{2,2}(E), SMatrix{1,1}(F))
     elseif M == 1 && N == 2
-        _sylvg!(A, B, C, E, F, Val(1), Val(2))
+        C[:] .= _sylvg(SMatrix{1,1}(A), SMatrix{2,2}(B), SMatrix{1,2}(C), SMatrix{1,1}(E), SMatrix{2,2}(F))
     elseif M == 1 && N == 1
-        _sylvg!(A, B, C, E, F, Val(1), Val(1))
+        C[:] .= _sylvg(SMatrix{1,1}(A), SMatrix{1,1}(B), SMatrix{1,1}(C), SMatrix{1,1}(E), SMatrix{1,1}(F))
     else
-        error("Matrix dimensionsins should not be greater than 2")
+        error("Problem size must not be greater than (2,2)")
     end
     return C
 end
-function _sylvg!(A::AbstractMatrix, B::AbstractMatrix, C::AbstractMatrix, E::AbstractMatrix, F::AbstractMatrix, ::Val{M}, ::Val{N}) where {T <: Number, M, N}
-    As = SMatrix{M,M}(A)
-    Bs = SMatrix{N,N}(B)
-    Es = SMatrix{M,M}(E)
-    Fs = SMatrix{N,N}(F)
-    Cvs = SMatrix{M,N}(C)[:] # vectorization of C
+function _sylvg(A::SMatrix, B::SMatrix, C::SMatrix, E::SMatrix, F::SMatrix)
 
-    Xv = (kron(transpose(Bs), As) + kron(transpose(Fs), Es)) \ Cvs # using the vectorization identity vec(AXB) = kron(B'*A)*vec(X)
+    Xv = (kron(transpose(B), A) + kron(transpose(F), E)) \ C[:] # using the vectorization identity vec(AXB) = kron(B'*A)*vec(X)
 
     if any(!isfinite, Xv); error("Matrix equation has no solution, see ?sylvg / ?lyapcg / ?lyapdg"); end
 
-    C .= reshape(Xv, M, N)
+    return Xv
 end
